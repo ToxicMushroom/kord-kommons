1650b8067ee9d93acd1b5b193588fed7f838eb57
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2021-09-12 12:18:21 +0200

    feat: allow publishing to private maven repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,17 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("jvm") version "1.5.30"
+    id("org.jetbrains.kotlin.jvm") version "1.5.30"
+    id("maven-publish")
 }
 
-group = "me.melijn"
+group = "me.melijn.kordkommons"
 version = "1.0"
+
+configure<JavaPluginExtension> {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
 
 repositories {
     mavenCentral()
@@ -16,4 +24,39 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+tasks {
+    withType(JavaCompile::class) {
+        options.encoding = "UTF-8"
+    }
+    withType(KotlinCompile::class) {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://nexus.melijn.com/repository/maven-releases/")
+            credentials {
+                username = property("melijnPublisher").toString()
+                password = property("melijnPassword").toString()
+            }
+        }
+    }
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            artifact(sourcesJar.get())
+        }
+    }
 }
