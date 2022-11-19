146116e568771bc15329c727eb1dcd3f02469b77
ToxicMushroom <32853531+toxicmushroom@users.noreply.github.com>
2022-11-19 22:43:56 +0100

    fix: add missing build.gradle file for buildSrc

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    google()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation(kotlin("gradle-plugin", version = "1.7.20"))
+    implementation(kotlin("serialization", version = "1.7.20"))
+
+    implementation("gradle.plugin.org.cadixdev.gradle", "licenser", "0.6.1")
+    implementation("com.github.jakemarsden", "git-hooks-gradle-plugin", "0.0.2")
+    implementation("com.google.devtools.ksp", "com.google.devtools.ksp.gradle.plugin", "1.7.20-1.0.6")
+    implementation("io.gitlab.arturbosch.detekt", "detekt-gradle-plugin", "1.19.0")
+    implementation("org.jetbrains.dokka", "dokka-gradle-plugin", "1.7.0")
+
+    implementation(gradleApi())
+    implementation(localGroovy())
+}
