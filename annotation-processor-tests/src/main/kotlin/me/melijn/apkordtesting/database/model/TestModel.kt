8761fe5720488400de3e0452563a0875307dc061
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2023-05-10 00:52:38 +0200

    fix: imports were missing

--- annotation-processor-tests/src/main/kotlin/me/melijn/apkordtesting/database/model/TestModel.kt
+++ annotation-processor-tests/src/main/kotlin/me/melijn/apkordtesting/database/model/TestModel.kt
@@ -3,6 +3,7 @@ package me.melijn.apkordtesting.database.model
 import me.melijn.apredgres.cacheable.Cacheable
 import me.melijn.apredgres.createtable.CreateTable
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.kotlin.datetime.date
 import org.jetbrains.exposed.sql.kotlin.datetime.datetime
 
 @CreateTable
@@ -12,10 +13,12 @@ object TestModel : Table("stuff") {
     val id = uuid("id")
     val name = text("name").nullable()
     val added = datetime("added")
+    val birthday = date("birthday")
 
     override val primaryKey = PrimaryKey(id)
 
     init {
         index(false, name)
+        index(false, birthday)
     }
 }
