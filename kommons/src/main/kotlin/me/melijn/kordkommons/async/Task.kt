d92191f23284d545ed91c0c5cfa6e4223ee4e939
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2023-05-18 01:33:30 +0200

    feat: reduce complexity of TaskManager, allow access to a supervisor coroutinescope

--- kommons/src/main/kotlin/me/melijn/kordkommons/async/Task.kt
+++ kommons/src/main/kotlin/me/melijn/kordkommons/async/Task.kt
@@ -1,74 +1,12 @@
 package me.melijn.kordkommons.async
 
-
-public class Task(private val func: suspend () -> Unit) : KTRunnable {
-
-    override suspend fun run() {
-        try {
-            func()
-        } catch (e: Throwable) {
-            e.printStackTrace()
-        }
-    }
-}
-
-public class DeferredNTask<T>(private val func: suspend () -> T?) : DeferredNKTRunnable<T> {
-
-    override suspend fun run(): T? {
-        return try {
-            func()
-        } catch (e: Throwable) {
-            e.printStackTrace()
-            null
-        }
-    }
-}
-
-public class EvalDeferredNTask<T>(private val func: suspend () -> T?) : DeferredNKTRunnable<T> {
-
-    override suspend fun run(): T? {
-        return func()
-    }
-}
-
-public class DeferredTask<T>(private val func: suspend () -> T) : DeferredKTRunnable<T> {
-
-    override suspend fun run(): T {
-        return func()
-    }
-}
+import kotlinx.coroutines.launch
 
 public class RunnableTask(private val func: suspend () -> Unit) : Runnable {
 
     override fun run() {
-        TaskManager.async {
+        TaskScope.launch {
             func()
         }
     }
-}
-
-public class TaskInline(private inline val func: () -> Unit) : Runnable {
-
-    override fun run() {
-        try {
-            func()
-        } catch (e: Throwable) {
-            e.printStackTrace()
-        }
-    }
-}
-
-@FunctionalInterface
-public interface KTRunnable {
-    public suspend fun run()
-}
-
-@FunctionalInterface
-public interface DeferredNKTRunnable<T> {
-    public suspend fun run(): T?
-}
-
-@FunctionalInterface
-public interface DeferredKTRunnable<T> {
-    public suspend fun run(): T
 }
--- kommons/src/main/kotlin/me/melijn/kordkommons/async/TaskManager.kt
+++ kommons/src/main/kotlin/me/melijn/kordkommons/async/TaskManager.kt
@@ -2,6 +2,7 @@ package me.melijn.kordkommons.async
 
 import kotlinx.coroutines.*
 import java.util.concurrent.*
+import kotlin.coroutines.CoroutineContext
 
 public object TaskManager {
 
@@ -12,52 +13,21 @@ public object TaskManager {
         }
     }
 
-    private val executorService: ExecutorService = ForkJoinPool()
-    @Suppress("MemberVisibilityCanBePrivate")
-    public val dispatcher: ExecutorCoroutineDispatcher = executorService.asCoroutineDispatcher()
     public var scheduledExecutorService: ScheduledExecutorService =
         Executors.newScheduledThreadPool(15, threadFactory.invoke("Repeater"))
-    public var coroutineScope: CoroutineScope = CoroutineScope(dispatcher)
-
-    public fun async(block: suspend CoroutineScope.() -> Unit): Job {
-        return coroutineScope.launch {
-            Task {
-                block.invoke(this)
-            }.run()
-        }
-    }
-
-    public fun asyncIgnoreEx(block: suspend CoroutineScope.() -> Unit): Job = coroutineScope.launch {
-        try {
-            block.invoke(this)
-        } catch (ignored: Throwable) {
-            // ignored by design
-        }
-    }
-
-    public fun <T> taskValueAsync(block: suspend CoroutineScope.() -> T): Deferred<T> = coroutineScope.async {
-        DeferredTask { block.invoke(this) }.run()
-    }
-
-    public fun <T> taskValueNAsync(block: suspend CoroutineScope.() -> T?): Deferred<T?> = coroutineScope.async {
-        DeferredNTask {
-            block.invoke(this)
-        }.run()
-    }
-
-    public fun <T> evalTaskValueNAsync(block: suspend CoroutineScope.() -> T?): Deferred<T?> = coroutineScope.async {
-        EvalDeferredNTask {
-            block.invoke(this)
-        }.run()
-    }
-
-    public inline fun asyncInline(crossinline block: CoroutineScope.() -> Unit): Job = coroutineScope.launch {
-        TaskInline {
-            block.invoke(this)
-        }.run()
-    }
 
     public inline fun asyncAfter(afterMillis: Long, crossinline func: suspend () -> Unit): ScheduledFuture<*> {
         return scheduledExecutorService.schedule(RunnableTask { func() }, afterMillis, TimeUnit.MILLISECONDS)
     }
+}
+
+public object TaskScope : CoroutineScope {
+
+    private val executorService: ExecutorService = ForkJoinPool()
+    public var taskManagerSupervisor: CompletableJob = SupervisorJob()
+    @Suppress("MemberVisibilityCanBePrivate")
+    public val dispatcher: ExecutorCoroutineDispatcher = executorService.asCoroutineDispatcher()
+
+    override val coroutineContext: CoroutineContext
+        get() = (dispatcher + taskManagerSupervisor)
 }
